eb7940ed35610fc34e63f9836cc9d6e736dfd559
Diego Lauar <diego_pktd@hotmail.com>
2024-09-24 13:31:16 -0300

    Add camada de dominio

--- src/controllers/fetch-recent-questions.controller.e2e-spec.ts
+++ src/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -4,6 +4,7 @@ import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+
 describe('Fetch recent questions (E2E)', () => {
     let app: INestApplication
     let prisma: PrismaService
